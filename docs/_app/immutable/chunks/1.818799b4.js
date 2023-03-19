cc258ed5cee6e63079c848ca69a4c1e66c910eec
itzjustalan <itzjustalan@gmail.com>
2023-03-20 02:09:10 +1300

    forgot gh page build

--- docs/_app/immutable/chunks/1.818799b4.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.541632c0.js";export{t as component};
---
+++ docs/_app/immutable/chunks/1.f19ecd61.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.aeafb26b.js";export{t as component};
